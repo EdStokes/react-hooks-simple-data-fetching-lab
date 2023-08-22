fde6e0183ea78b643bb8f5600c2edb0e9924365f
Ed Stokes <edstokes96@gmail.com>
2023-08-22 07:35:53 -0500

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,26 @@
-// create your App component here
+import React, { useState, useEffect } from "react";
+
+function App() {
+    const API_URL = "https://dog.ceo/api/breeds/image/random";
+    const [img, setImg] = useState();
+
+    useEffect(() => {
+        fetch(API_URL)
+            .then((response) => response.json())
+            .then((data) => {
+                setImg(data.message);
+            });
+    }, []);
+
+    if (!img) {
+        return <h2>Loading...</h2>
+    }
+
+    return (
+        <img src={img} alt="A Random Dog" />
+
+    )
+
+}
+
+export default App;
